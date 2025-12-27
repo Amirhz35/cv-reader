2c3a67363de0c58cc6e594277f69df1fd3e7772d
amirhossein <zamir1335@gamil.com>
2025-12-27 16:48:43 +0330

    .

--- cv_screening/settings.py
+++ cv_screening/settings.py
@@ -211,6 +211,10 @@ SIMPLE_JWT = {
 CORS_ALLOWED_ORIGINS = [
     "http://localhost:3000",
     "http://127.0.0.1:3000",
+    "http://ats-ui.liara.run",
+    "https://ats-ui.liara.run",
+    "http://www.ats-ui.liara.run",
+    "https://www.ats-ui.liara.run",
 ]
 
 # File storage settings
